54e461ccb5cab4c0b1388f03af59b9de77d11da5
ahmadzen022 <141709205+ahmadzen022@users.noreply.github.com>
2023-08-08 13:50:06 +0700

    Add files via upload

---
+++ guess.py
@@ -0,0 +1,9 @@
+class tebak:
+    def __init__(self):
+        self.tebakan = "tebakan"
+        self.jawaban = "jawaban"
+
+    def cek(self):
+        if self.tebakan == self.jawaban:
+            return True
+        return False
---
+++ jobsheet1.py
@@ -0,0 +1,9 @@
+import random
+from guess import tebak
+
+jawaban = random.randint(1, 10)
+tebak = int((input('tebak angka 1 s.d 10:')))
+if ():
+    print("jawaban kamu benar")
+else:
+    print("jawaban kamu salah")
